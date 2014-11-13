c8f75625242339a7451e67b9a649bc96dfa088bc
Richo Healey <richo@psych0tik.net>
2014-11-12 20:29:57 -0800

    Refactor incoming

---
+++ src/incoming.rs
@@ -0,0 +1,50 @@
+use std::str;
+use std::collections::HashMap;
+
+#[deriving(Show)]
+pub struct SlackPayload<'a> {
+    token: &'a str,
+    team_id: &'a str,
+    channel_id: &'a str,
+    channel_name: &'a str,
+    user_id: &'a str,
+    pub user_name: &'a str,
+    pub command: &'a str,
+    text: &'a str,
+}
+
+macro_rules! get(
+    ($hash: expr, $opt:expr, $msg:expr) => {
+        match $hash.get(&$opt) {
+            Some(n) => *n,
+            None => return Err($msg.to_string()),
+        }
+    }
+)
+
+impl<'a> SlackPayload<'a> {
+    pub fn from_body<'a>(req: &[u8]) -> Result<SlackPayload, String> {
+        let pieces = req.split(|c| *c == '&' as u8);
+        let pieces: Vec<&str> = pieces.map(|c| str::from_utf8(c).unwrap()).collect();
+        let mut vars = HashMap::new();
+
+        for c in pieces.iter() {
+            let mut l1 = c.splitn(1, '=');
+            match (l1.next(), l1.next()) {
+                (Some(m1), Some(m2)) => { vars.insert(m1, m2); },
+                _ => {},
+            }
+        }
+
+        return Ok(SlackPayload {
+            token:          get!(vars, "token", "No `token` in request"),
+            team_id:        get!(vars, "team_id", "No `team_id` in request"),
+            channel_id:     get!(vars, "channel_id", "No `channel_id` in request"),
+            channel_name:   get!(vars, "channel_name", "No `channel_name` in request"),
+            user_id:        get!(vars, "user_id", "No `user_id` in request"),
+            user_name:      get!(vars, "user_name", "No `user_name` in request"),
+            command:        get!(vars, "command", "No `command` in request"),
+            text:           get!(vars, "text", "No `text` in request"),
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ extern crate url;
 extern crate serialize;
 
 use std::os;
-use std::str;
 use std::sync::{Arc, Mutex};
 use std::io::net::ip::{SocketAddr, Ipv4Addr};
 use std::collections::HashMap;
@@ -15,8 +14,10 @@ use http::method::{Get, Post};
 use http::server::{Config, Server, Request, ResponseWriter};
 use http::server::request::{AbsolutePath};
 
+use incoming::{SlackPayload};
 use outgoing::{SlackEndpoint, OutgoingWebhook};
 
+mod incoming;
 mod outgoing;
 
 type Scores = HashMap<String, i32>;
@@ -33,54 +34,6 @@ impl KarmaServer {
             scores: Arc::new(Mutex::new(scores)),
             endpoint: endpoint
         }
-    }
-}
-
-#[deriving(Show)]
-struct SlackPayload<'a> {
-    token: &'a str,
-    team_id: &'a str,
-    channel_id: &'a str,
-    channel_name: &'a str,
-    user_id: &'a str,
-    user_name: &'a str,
-    command: &'a str,
-    text: &'a str,
-}
-
-macro_rules! get(
-    ($hash: expr, $opt:expr, $msg:expr) => {
-        match $hash.get(&$opt) {
-            Some(n) => *n,
-            None => return Err($msg.to_string()),
-        }
-    }
-)
-
-impl<'a> SlackPayload<'a> {
-    fn from_body<'a>(req: &[u8]) -> Result<SlackPayload, String> {
-        let pieces = req.split(|c| *c == '&' as u8);
-        let pieces: Vec<&str> = pieces.map(|c| str::from_utf8(c).unwrap()).collect();
-        let mut vars = HashMap::new();
-
-        for c in pieces.iter() {
-            let mut l1 = c.splitn(1, '=');
-            match (l1.next(), l1.next()) {
-                (Some(m1), Some(m2)) => { vars.insert(m1, m2); },
-                _ => {},
-            }
-        }
-
-        return Ok(SlackPayload {
-            token:          get!(vars, "token", "No `token` in request"),
-            team_id:        get!(vars, "team_id", "No `team_id` in request"),
-            channel_id:     get!(vars, "channel_id", "No `channel_id` in request"),
-            channel_name:   get!(vars, "channel_name", "No `channel_name` in request"),
-            user_id:        get!(vars, "user_id", "No `user_id` in request"),
-            user_name:      get!(vars, "user_name", "No `user_name` in request"),
-            command:        get!(vars, "command", "No `command` in request"),
-            text:           get!(vars, "text", "No `text` in request"),
-        })
     }
 }
 
